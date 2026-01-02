b8693d1f2ec97b9965b840f2dcf3ef58d27f74b0
Harsh Raj <harshhraj2468@gmail.com>
2026-01-02 18:52:24 +0530

    new logic for streak implemented

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 window.onload = spicyPotatoStats;
 
+
+let HarshStreak = 1;
+let RaunakStreak = 1;
+let PrathamStreak = 1;
+let PrahladStreak = 1;
+
+
 async function spicyPotatoStats() {
   const loader = document.getElementById("loader");
   const container = document.querySelector(".container");
@@ -10,19 +17,10 @@ async function spicyPotatoStats() {
   footer.style.visibility = "hidden";
 
   try {
-    await Promise.all([
-      fetchInfo("vorqx", "Harsh"),
-      fetchInfo("rust7", "Raunak"),
-      fetchInfo("sn0w07", "Pratham"),
-      fetchInfo("prahlad_kumar-0037", "Prahlad"),
-    ]);
+    await fetchInfoAll();
 
-    await Promise.all([
-      fetchStreak("vorqx", "HarshStreak"),
-      fetchStreak("rust7", "RaunakStreak"),
-      fetchStreak("sn0w07", "PrathamStreak"),
-      fetchStreak("prahlad_kumar-0037", "PrahladStreak"),
-    ]);
+    await fetchStreak();
+    
   } catch (err) {
     loader.innerHTML = "Failed to load!";
   } finally {
@@ -34,106 +32,337 @@ async function spicyPotatoStats() {
 
 const tem = document.getElementById("time");
 
-setInterval(() => {
-  const tim = new Date().toLocaleTimeString();
-  tem.innerHTML = `<h1>Time: <span> ${tim} </span> <h1>`;
-}, 1000);
 
-async function fetchInfo(userName, specialId) {
-  const info = await fetch(
-    `https://codeforces.com/api/user.info?handles=${userName}`
-  );
-  const data = await info.json();
 
-  const user = data.result[0];
-  document.getElementById(
-    `${specialId}userName`
-  ).innerHTML = `<p> ${user.handle} </p>`;
-  document.getElementById(
-    `${specialId}rating`
-  ).innerHTML = `<p> ${user.rating} </p>`;
-  document.getElementById(
-    `${specialId}pfp`
-  ).style.backgroundImage = `url(${user.avatar})`;
+function displayTime() {
+ 
+    const currentTime = new Date();
 
-  const lastOnline = new Date(user.lastOnlineTimeSeconds * 1000).toLocaleString(
-    "en-IN",
-    {
-      dateStyle: "medium",
-      timeStyle: "short",
-      timeZone: "Asia/Kolkata",
-    }
-  );
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    
 
-  document.getElementById(
-    `${specialId}lastonline`
-  ).innerHTML = `<p> ${lastOnline} </p>`;
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; 
+
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    const timeString = `${hours}:${minutes}:${seconds} ${ampm}`;
+
+    document.getElementById('yo').textContent = timeString;
 }
 
-async function fetchStreak(userName, specialId) {
-  const STREAK_START = new Date("2026-01-01").toLocaleDateString();
-  const today = new Date().toLocaleDateString();
+displayTime();
 
-  const streak = await fetch(
-    `https://codeforces.com/api/user.status?handle=${userName}&from=1&count=20`
+setInterval(displayTime, 1000);
+
+
+
+
+async function fetchInfoAll() {
+   const info = await fetch("https://codeforces.com/api/user.info?handles=vorqX;rust7;sn0w07;prahlad_kumar-0037");
+         const data = await info.json();
+         
+         const dataArr = data.result;
+         const harshData = dataArr[0];
+         const raunakData = dataArr[1];
+         const prathamData = dataArr[2];
+         const prahladData = dataArr[3];
+         
+        document.getElementById("HarshuserName").innerHTML = `<p> ${harshData.handle} </p>`
+        document.getElementById("RaunakuserName").innerHTML = `<p> ${raunakData.handle} </p>`
+        document.getElementById("PrathamuserName").innerHTML = `<p> ${prathamData.handle} </p>`
+        document.getElementById("PrahladuserName").innerHTML = `<p> ${prahladData.handle} </p>`
+         
+        document.getElementById("Harshrating").innerHTML = `<p> ${harshData.rating} </p>`
+        document.getElementById("Raunakrating").innerHTML = `<p> ${raunakData.rating} </p>`
+        document.getElementById("Prathamrating").innerHTML = `<p> ${prathamData.rating} </p>`
+        document.getElementById("Prahladrating").innerHTML = `<p> ${prahladData.rating} </p>`
+
+        document.getElementById("Harshpfp").style.backgroundImage = `url(${harshData.avatar})`
+        document.getElementById("Raunakpfp").style.backgroundImage = `url(${raunakData.avatar})`
+        document.getElementById("Prathampfp").style.backgroundImage = `url(${prathamData.avatar})`
+        document.getElementById("Prahladpfp").style.backgroundImage = `url(${prahladData.avatar})`
+
+
+        const HarshlastOnline = new Date(harshData.lastOnlineTimeSeconds * 1000).toLocaleString("en-IN", {
+                   dateStyle: "medium",
+                   timeStyle: "short",
+                   timeZone: "Asia/Kolkata"
+                })
+         
+        const RaunaklastOnline = new Date(raunakData.lastOnlineTimeSeconds * 1000).toLocaleString("en-IN", {
+                   dateStyle: "medium",
+                   timeStyle: "short",
+                   timeZone: "Asia/Kolkata"
+                })
+        
+        const PrathamlastOnline = new Date(prathamData.lastOnlineTimeSeconds * 1000).toLocaleString("en-IN", {
+                   dateStyle: "medium",
+                   timeStyle: "short",
+                   timeZone: "Asia/Kolkata"
+                })
+        
+        const PrahladlastOnline = new Date(prahladData.lastOnlineTimeSeconds * 1000).toLocaleString("en-IN", {
+                   dateStyle: "medium",
+                   timeStyle: "short",
+                   timeZone: "Asia/Kolkata"
+                })        
+      
+        document.getElementById("Harshlastonline").innerHTML = `<p> ${HarshlastOnline} </p>`
+        document.getElementById("Raunaklastonline").innerHTML = `<p> ${RaunaklastOnline} </p>`
+        document.getElementById("Prathamlastonline").innerHTML = `<p> ${PrathamlastOnline} </p>`
+        document.getElementById("Prahladlastonline").innerHTML = `<p> ${PrahladlastOnline} </p>`
+
+}
+
+
+async function fetchStreak() {
+  const STREAK_START = new Date("2026-01-01").setHours(0,0,0,0);
+  const today = new Date();
+  today.setHours(0,0,0,0);
+
+  const harshstreak = await fetch(
+    `https://codeforces.com/api/user.status?handle=vorqx&from=1&count=20`
   );
-  const streakData = await streak.json();
+  const harshstreakData = await harshstreak.json();
 
-  const streakUpdate = streakData.result;
+  const harshstreakUpdate = harshstreakData.result;
 
-  let streakValue = 0;
-  const solvedDays = new Set();
+  const harshsolvedDays = new Set();
 
-  streakUpdate.forEach((e) => {
+  harshstreakUpdate.forEach((e) => {
     if (e.verdict === "OK") {
-      const meo = new Date(e.creationTimeSeconds * 1000).toLocaleDateString();
-      solvedDays.add(meo);
+      const meo = new Date(e.creationTimeSeconds * 1000).setHours(0,0,0,0);
+      harshsolvedDays.add(meo);
     }
   });
 
-  const array = Array.from(solvedDays);
-  array.sort((a, b) => new Date(b) - new Date(a));
+  const harsharray = Array.from(harshsolvedDays);
+  harsharray.sort((a, b) => new Date(b) - new Date(a));
 
-  const filteredArray = array.filter(
-    (d) => new Date(d) >= new Date(STREAK_START)
-  );
+  const harshfilteredArray = harsharray.filter((d) => new Date(d) >= new Date(STREAK_START));
 
-  if (Date.parse(today) < Date.parse(STREAK_START)) {
-    document.getElementById(specialId).parentElement.style.display = "none";
-  } else if (filteredArray.length === 0) {
-    document.getElementById(specialId).parentElement.style.display = "none";
-  } else {
-    const latest = new Date(filteredArray[0]);
-    const now = new Date();
+  if (harshfilteredArray.length === 0) {
+       document.getElementById(`HarshStreak`).parentElement.style.display = "none";
+       return;
+   }
 
-    latest.setHours(0, 0, 0, 0);
-    now.setHours(0, 0, 0, 0);
+  if (harshfilteredArray.length === 1) {
+      document.getElementById(`HarshStreak`).innerHTML = `<span>1</span>`;
+      document.getElementById(`HarshStreak`).parentElement.style.display = "flex";
+      return;
+   }
+     
+     
+     let harshLatestSolvedDate = new Date(harshfilteredArray[0]);
+     let harshpreviousSolvedDate = new Date(harshfilteredArray[1]);
 
-    const diffDays = Math.floor((now - latest) / (24 * 60 * 60 * 1000));
+     harshLatestSolvedDate.setHours(0,0,0,0);
+     harshpreviousSolvedDate.setHours(0,0,0,0);
 
-    if (diffDays > 1) {
-      document.getElementById(specialId).parentElement.style.display = "none";
-    } else {
-      streakValue = 1;
+     const harshgap = harshLatestSolvedDate - harshpreviousSolvedDate;
+     const harshgapDays = Math.floor( harshgap/ (24*60*60*1000));
+  
 
-      for (let i = 0; i < filteredArray.length - 1; i++) {
-        let d1 = new Date(filteredArray[i]);
-        let d2 = new Date(filteredArray[i + 1]);
-
-        d1.setHours(0, 0, 0, 0);
-        d2.setHours(0, 0, 0, 0);
-
-        let gap = Math.floor(Math.abs(d1 - d2) / (24 * 60 * 60 * 1000));
-
-        if (gap === 1) {
-          streakValue++;
-        } else break;
-      }
-
-      document.getElementById(
-        specialId
-      ).innerHTML = `<span>${streakValue}</span>`;
-      document.getElementById(specialId).parentElement.style.display = "flex";
+     if( harshgapDays > 1){
+      document.getElementById(`HarshStreak`).parentElement.style.display = "none";
+     } else {
+         if( today.getTime() === harshLatestSolvedDate.getTime() ){
+            HarshStreak++;
+            document.getElementById(`HarshStreak`).innerHTML = `<span>${HarshStreak}</span>`;
+            document.getElementById(`HarshStreak`).parentElement.style.display = "flex";
+          
+         } else {
+             document.getElementById(`HarshStreak`).parentElement.style.display = "none";
+         }
     }
-  }
+
+
+    const raunakstreak = await fetch(
+    `https://codeforces.com/api/user.status?handle=rust7&from=1&count=20`
+  );
+  const raunakstreakData = await raunakstreak.json();
+
+  const raunakstreakUpdate = raunakstreakData.result;
+
+  const raunaksolvedDays = new Set();
+
+  raunakstreakUpdate.forEach((e) => {
+    if (e.verdict === "OK") {
+      const meo = new Date(e.creationTimeSeconds * 1000).setHours(0,0,0,0);
+      raunaksolvedDays.add(meo);
+    }
+  });
+
+  const raunakarray = Array.from(raunaksolvedDays);
+  raunakarray.sort((a, b) => new Date(b) - new Date(a));
+
+  const raunakfilteredArray = raunakarray.filter((d) => new Date(d) >= new Date(STREAK_START));
+
+
+  if (raunakfilteredArray.length === 0) {
+       document.getElementById(`RaunakStreak`).parentElement.style.display = "none";
+       return;
+   }
+
+  if (raunakfilteredArray.length === 1) {
+      document.getElementById(`RaunakStreak`).innerHTML = `<span>1</span>`;
+      document.getElementById(`RaunakStreak`).parentElement.style.display = "flex";
+      return;
+   }
+     
+     
+     let raunakLatestSolvedDate = new Date(raunakfilteredArray[0]);
+     let raunakpreviousSolvedDate = new Date(raunakfilteredArray[1]);
+
+     raunakLatestSolvedDate.setHours(0,0,0,0);
+     raunakpreviousSolvedDate.setHours(0,0,0,0);
+
+     const raunakgap = raunakLatestSolvedDate - raunakpreviousSolvedDate;
+     const raunakgapDays = Math.floor( raunakgap/ (24*60*60*1000));
+  
+
+     if( raunakgapDays > 1){
+      document.getElementById(`RaunakStreak`).parentElement.style.display = "none";
+     } else {
+         if( today.getTime() === raunakLatestSolvedDate.getTime() ){
+              RaunakStreak++;
+            document.getElementById(`RaunakStreak`).innerHTML = `<span>${RaunakStreak}</span>`;
+            document.getElementById(`RaunakStreak`).parentElement.style.display = "flex";
+          
+         } else {
+             document.getElementById(`RaunakStreak`).parentElement.style.display = "none";
+         }
+    }
+
+
+
+    const prathamDatastreak = await fetch(
+    `https://codeforces.com/api/user.status?handle=sn0w07&from=1&count=20`
+  );
+  const prathamDatastreakData = await prathamDatastreak.json();
+
+  const prathamDatastreakUpdate = prathamDatastreakData.result;
+
+  const prathamDatasolvedDays = new Set();
+
+  prathamDatastreakUpdate.forEach((e) => {
+    if (e.verdict === "OK") {
+      const meo = new Date(e.creationTimeSeconds * 1000).setHours(0,0,0,0);
+      prathamDatasolvedDays.add(meo);
+    }
+  });
+
+  const prathamDataarray = Array.from(prathamDatasolvedDays);
+  prathamDataarray.sort((a, b) => new Date(b) - new Date(a));
+
+  const prathamDatafilteredArray = prathamDataarray.filter((d) => new Date(d) >= new Date(STREAK_START));
+
+
+  if (prathamDatafilteredArray.length === 0) {
+       document.getElementById(`PrathamStreak`).parentElement.style.display = "none";
+       return;
+   }
+
+  if (prathamDatafilteredArray.length === 1) {
+      document.getElementById(`PrathamStreak`).innerHTML = `<span>1</span>`;
+      document.getElementById(`PrathamStreak`).parentElement.style.display = "flex";
+      return;
+   }
+     
+     
+     let prathamDataLatestSolvedDate = new Date(prathamDatafilteredArray[0]);
+     let prathamDatapreviousSolvedDate = new Date(prathamDatafilteredArray[1]);
+
+     prathamDataLatestSolvedDate.setHours(0,0,0,0);
+     prathamDatapreviousSolvedDate.setHours(0,0,0,0);
+
+     const prathamDatagap = prathamDataLatestSolvedDate - prathamDatapreviousSolvedDate;
+     const prathamDatagapDays = Math.floor(prathamDatagap/ (24*60*60*1000));
+  
+       console.log(prathamDataLatestSolvedDate)
+
+
+     if( prathamDatagapDays > 1){
+      document.getElementById(`PrathamStreak`).parentElement.style.display = "none";
+     } else {
+         if( today.getTime() === prathamDataLatestSolvedDate.getTime() ){
+              PrathamStreak++;
+            document.getElementById(`PrathamStreak`).innerHTML = `<span>${PrathamStreak}</span>`;
+            document.getElementById(`PrathamStreak`).parentElement.style.display = "flex";
+          
+         } else {
+             document.getElementById(`PrathamStreak`).parentElement.style.display = "none";
+         }
+    }
+
+    
+     const prahladDatastreak = await fetch(
+    `https://codeforces.com/api/user.status?handle=prahlad_kumar-0037&from=1&count=20`
+  );
+  const prahladDatastreakData = await prahladDatastreak.json();
+
+  const prahladDatastreakUpdate = prahladDatastreakData.result;
+
+  const prahladDatasolvedDays = new Set();
+
+  prahladDatastreakUpdate.forEach((e) => {
+    if (e.verdict === "OK") {
+      const meo = new Date(e.creationTimeSeconds * 1000).setHours(0,0,0,0);
+      prahladDatasolvedDays.add(meo);
+    }
+  });
+
+  const prahladDataarray = Array.from(prahladDatasolvedDays);
+  prahladDataarray.sort((a, b) => new Date(b) - new Date(a));
+
+  const prahladDatafilteredArray = prahladDataarray.filter((d) => new Date(d) >= new Date(STREAK_START));
+
+
+  if (prahladDatafilteredArray.length === 0) {
+       document.getElementById(`PrahladStreak`).parentElement.style.display = "none";
+       return;
+   }
+
+  if (prahladDatafilteredArray.length === 1) {
+      document.getElementById(`PrahladStreak`).innerHTML = `<span>1</span>`;
+      document.getElementById(`PrahladStreak`).parentElement.style.display = "flex";
+      return;
+   }
+     
+     
+     let prahladDataLatestSolvedDate = new Date(prahladDatafilteredArray[0]);
+     let prahladDatapreviousSolvedDate = new Date(prahladDatafilteredArray[1]);
+
+     prahladDataLatestSolvedDate.setHours(0,0,0,0);
+     prahladDatapreviousSolvedDate.setHours(0,0,0,0);
+
+     const prahladDatagap = prahladDataLatestSolvedDate - prahladDatapreviousSolvedDate;
+     const prahladDatagapDays = Math.floor(prahladDatagap/ (24*60*60*1000));
+  
+
+     if( prahladDatagapDays > 1){
+      document.getElementById(`PrahladStreak`).parentElement.style.display = "none";
+     } else {
+         if( today.getTime() === prahladDataLatestSolvedDate.getTime() ){
+              PrahladStreak++;
+            document.getElementById(`PrahladStreak`).innerHTML = `<span>${PrahladStreak}</span>`;
+            document.getElementById(`PrahladStreak`).parentElement.style.display = "flex";
+          
+         } else {
+             document.getElementById(`PrahladStreak`).parentElement.style.display = "none";
+         }
+    }
+
+
+
+
+
+
+
+
 }
